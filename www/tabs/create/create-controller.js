1a5f9c26ab56cd4a14bd8b5f891fad7e24178568
baoducng <baoducng@gmail.com>
2014-08-22 09:51:31 -0700

    Revert "Feat/lord have mercy"

--- www/tabs/create/create-controller.js
+++ www/tabs/create/create-controller.js
@@ -28,15 +28,14 @@
           },
           buttonClicked: function(index, data) {
             if (index === 3) {
-              self.showTagsPopup();
+              self.showPopup();
             } else {
               self.data.currentTags[data.text] = true;
             }
           }
         });
       };
-
-      this.showTagsPopup = function() {
+      this.showPopup = function() {
         var myPopup = $ionicPopup.show({
           template: '<input type="text" ng-model="create.data.newTag">',
           title: 'Enter a new tag',
@@ -56,33 +55,11 @@
         });
       };
 
-      this.showTextPopup = function() {
-        var myPopup = $ionicPopup.show({
-            template: '<input type="text" ng-model="create.data.textContent">',
-            title: 'Enter text content',
-            scope: $scope,
-            buttons: [{
-            text: 'Cancel'
-          }, {
-            text: '<b>Save text</b>',
-            type: 'button-positive',
-            onTap: function(e) {
-              // self.data.textContent = true;
-            }
-          }, ]
-        });
-        myPopup.then(function(res) {
-        });
-      };
-
 
       this.saveStache = function() {
-        this.s3Upload();
-        console.log('self.data: ');
-        console.dir(self.data);
         stacheService.saveStache({
           title: self.data.titleText,
-          content: self.data.textContent,
+          content: self.data.content,
           tags: Object.keys(self.data.currentTags),
           lon: self.location.long,
           lat: self.location.lat,
@@ -107,38 +84,6 @@
         }, function(err) {
           self.audioData = err;
         });
-      };
-
-      this.s3Upload = function() {
-        // See status of upload (% complete, public url)
-        var status_elem = document.getElementById("status");
-        var url_elem = document.getElementById("avatar_url");
-        var preview_elem = document.getElementById("preview");
-        var rString = this.randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-
-        var s3upload = new S3Upload({
-          s3_object_name: rString,
-          file_dom_selector: 'files',
-          s3_sign_put_url: 'http://specter.azurewebsites.net/staches/sign_s3/',
-          onProgress: function(percent, message) {
-              status_elem.innerHTML = 'Upload progress: ' + percent + '% ' + message;
-          },
-          onFinishS3Put: function(public_url) {
-              status_elem.innerHTML = 'Upload completed. Uploaded to: ' + public_url;
-              url_elem.value = public_url;
-              // Store this url in mongodb
-              preview_elem.innerHTML = '<img src="' + public_url + '" style="width:100px;"/>';
-          },
-          onError: function(status) {
-              status_elem.innerHTML = 'Upload error: ' + status;
-          }
-        });
-      };
-
-      this.randomString = function(length, chars) {
-        var result = '';
-        for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-        return result;
       };
 
       $ionicModal.fromTemplateUrl('private-modal.html', {
