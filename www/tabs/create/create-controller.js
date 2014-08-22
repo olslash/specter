26dacc66807bd40f2b3a495377cb35ac937e0d0c
valiantvu <michelle.thi.vu@gmail.com>
2014-08-22 15:11:57 -0700

    (feat) Change Stache button to trigger s3upload, which then invokes saveStache

--- www/tabs/create/create-controller.js
+++ www/tabs/create/create-controller.js
@@ -1,7 +1,6 @@
 (function(){
     var createCtrl = function($ionicActionSheet, $ionicPopup, Restangular, cameraService,
-      stacheService, $cordovaCapture, $cordovaGeolocation, geoService, $ionicModal, $scope, ngProgress) {
-      var progressBar = ngProgress;
+      stacheService, $cordovaCapture, $cordovaGeolocation, geoService, $ionicModal, $scope) {
       var self = this;
       this.data = {
         currentTags: {}
@@ -122,7 +121,6 @@
           s3_sign_put_url: 'http://specter.azurewebsites.net/staches/sign_s3/',
           onProgress: function(percent, message) {
               status_elem.innerHTML = 'Upload progress: ' + percent + '% ' + message;
-              // progressBar.set(percent);
           },
           onFinishS3Put: function(public_url) {
               status_elem.innerHTML = 'Upload completed. Uploaded to: ' + public_url;
@@ -130,7 +128,7 @@
               // Store this url in mongodb
               self.data.aws_url = public_url;
               self.saveStache();
-              preview_elem.innerHTML = '<img src="' + public_url + '" style="height:45px;border: #455059 4px solid;"/>';
+              preview_elem.innerHTML = '<img src="' + public_url + '" style="width:100px;"/>';
           },
           onError: function(status) {
               status_elem.innerHTML = 'Upload error: ' + status;
